991c12a7d2749f6bcd0cc91ca28f755f0f239aff
TaeXtream <59652919+TaeXtream@users.noreply.github.com>
2021-03-06 08:16:48 +0700

    Add Model for adding data to system

---
+++ src/main/java/io/MUIC/BlockChain/ProjectBackEnd/Model/AddCustomerRequest.java
@@ -0,0 +1,22 @@
+package io.MUIC.BlockChain.ProjectBackEnd.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class AddCustomerRequest {
+
+    private String username;
+
+    private String password;
+
+    private String firstname;
+
+    private String lastname;
+
+}
---
+++ src/main/java/io/MUIC/BlockChain/ProjectBackEnd/Model/AddPropertyRequest.java
@@ -0,0 +1,31 @@
+package io.MUIC.BlockChain.ProjectBackEnd.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddPropertyRequest {
+
+    private String name;
+
+    private String addressNumber;
+
+    private String district;
+
+    private String province;
+
+    private String country;
+
+    private boolean status;
+
+    private String buildingType;
+
+    private String salePrice;
+
+    private String rentPrice;
+}
